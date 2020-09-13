788e23a844baa2144b8c17b301aa3dc1a2d2de9d
Maverick Delta <68787583+MaverickDelta@users.noreply.github.com>
2020-09-13 16:47:05 +0100

    Create fxmanifest.lua

---
+++ ChatCommands/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'bodacious'
+games {'gta5'}
+
+author 'Maverick Delta'
+description 'Chat System'
+version '1.0.0'
+
+-- Script 1/2
+client_script 'CommandsClient.lua'
+server_script 'CommandsServer.lua'
